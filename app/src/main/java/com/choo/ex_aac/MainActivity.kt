12be6a4b35efaf20f74610a02ffed615c5bd4af3
ChooJeongHo <kd64choo@gmail.com>
2025-01-10 21:40:02 +0900

    뷰 바인딩 수정 및 적용

--- app/src/main/java/com/choo/ex_aac/MainActivity.kt
+++ app/src/main/java/com/choo/ex_aac/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.choo.ex_aac
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.choo.ex_aac.databinding.ActivityMainBinding
 
 
--- app/src/main/java/com/choo/ex_aac/TodoAdapter.kt
+++ app/src/main/java/com/choo/ex_aac/TodoAdapter.kt
@@ -1,10 +1,7 @@
 package com.choo.ex_aac
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
